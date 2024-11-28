43cf1e9b1e39b8fa7550c75c6e99522846bef4b9
Shaheer Ahmed <shaheer.ahmed@dig8labs.com>
2024-11-28 13:22:11 +0500

    CSI Test Commit

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,21 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
 
-export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "gatsby",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+export default{
+    contentSources: [
+        new ContentfulContentSource({
+          spaceId: process.env.CONTENTFUL_SPACE_ID,
+          environment: process.env.CONTENTFUL_ENVIRONMENT,
+          previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+          accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
+        })
+  ]
+} 
+
+// defineStackbitConfig({
+//     "stackbitVersion": "~0.6.0",
+//     "nodeVersion": "18",
+//     "ssgName": "gatsby",
+//     "contentSources": [],
+//     "postInstallCommand": "npm i --no-save @stackbit/types"
+// })
